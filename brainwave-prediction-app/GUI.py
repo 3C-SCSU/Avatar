360cbd4dd977912d6042a9d86d50a0b92e308824
j-knudson <knudsonjd@gmail.com>
2023-04-30 09:19:14 -0500

    Added relative pathing in server.py for model; updated GUI.py to use tables

--- brainwave-prediction-app/GUI.py
+++ brainwave-prediction-app/GUI.py
@@ -5,12 +5,15 @@ from client.brainflow1 import bciConnection
 
 #TODO enable imports
 #tello imports
-# from djitellopy import Tello
-# tello = Tello()
+from djitellopy import Tello
+tello = Tello()
 
-def get_drone_action(action): ##receives action from a GUI button, executes a corresponding Tello-Drone class move action, then returns "Done"
+def get_drone_action(action): ##receives action from a GUI button and executes a corresponding Tello-Drone class move action, then returns "Done"
 
-    if action == 'backward':
+    if action == 'connect':
+        #tello.connect()
+        print("tello.connect()")
+    elif action == 'backward':
         #tello.move_back(30)
         print('tello.move_back(30)')           
     elif action == 'down':
@@ -29,19 +32,19 @@ def get_drone_action(action): ##receives action from a GUI button, executes a co
         #tello.move_right(30)
         print('tello.move_right(30)')
     elif action == 'takeoff':
-        #tello.takeoff(30)
+        #tello.takeoff
         print('tello.takeoff')
     elif action == 'up':
         #tello.move_up(30)
         print('tello.move_up(30)')
     elif action == 'turn_left':
-        #tello.rotate_ccw(45)
-        print('tello.rotate_ccw(45)')
+        #tello.rotate_counter_clockwise(45)
+        print('tello.rotate_counter_clockwise(45)')
     elif action == 'turn_right':
-        #tello.rotate_cw(45)
-        print('tello.rotate_cw(45)')
+        #tello.rotate_clockwise(45)
+        print('tello.rotate_clockwise(45)')
     elif action == 'flip': 
-        #tello.flip('b')
+        #tello.flip_back()
         print("tello.flip('b')")
     
     #TODO Remove sleep
@@ -58,7 +61,7 @@ def drone_holding_pattern():
     in_pattern = False
     return (in_pattern) #let calling Window know if it needs to restart Holding Pattern
 def use_brainflow(): 
-    print("I'm in use brainflow")
+    #print("I'm in use brainflow")
     #Create BCI object
     bci = bciConnection()
     server_response = bci.bciConnectionController()
@@ -103,22 +106,48 @@ def holding_pattern_window():
 
 def brainwave_prediction_window():
     #Layout
+    flight_log = [] #array to hold flight log info
+    predictions_log = [] #array to hold info for table
+    predictions_headings = ['Predictions Count', 'Server Predictions', 'Prediction Label'] # table headings 
+    response_headings = ['Count', 'Label']
+    top_left = [    
+                    [sg.Radio('Manual Control', 'pilot', default=True, size=(-20, 1)), sg.Radio('Autopilot', 'pilot',  size=(12, 1))],
+                    [sg.Button('Read my mind...', size=(40,5), image_filename="./images/brain.png" )], 
+                    #[sg.Text(key="-COUNT-"), sg.Text(key="-PREDICTION-")], 
+                    [sg.Text("The model says ...")], 
+                    [sg.Table (values=[], headings=response_headings,  auto_size_columns=False, def_col_width=15, justification='center', num_rows=1, key='-SERVER_TABLE-', row_height=25, tooltip="Server Response Table", hide_vertical_scroll=True,)],
+
+                    [sg.Button('Not what I was thinking...', size=(14,3)), sg.Button('Execute', size=(14,3)), sg.Push()],  
+               ]
+    bottom_left = [
+                    [sg.Text('Flight Log')],[sg.Listbox(values=flight_log, size=(30, 6), key='LOG')],
+                  ]
+    bottom_right = [
+                    [sg.Text('Console Log')],
+                    [sg.Output(s=(45,10))]  
+                   ]
+
     brainwave_prediction_layout = [                                    
-                                    [sg.Text('Flight Log')], [sg.Listbox(values=[], size=(30, 6), key='LABELS')],
-
-                                    [sg.Text('This is count number:'), sg.Text('Do you want to...?:')], [sg.Text(key="-COUNT-"), sg.Text(key="-PREDICTION-")],                          
-                            
-
-                                    [sg.Button('Not what I was thinking...', size=(14,3)), sg.Button('Execute', size=(14,3))],  
-
-                                    [sg.Button('Read my mind...', size=(40,5))],
-
-                                    [sg.Text('Flight Log2')],
-                                    [sg.Output(s=(45,10))]                                 
+                                    
+                                    [sg.Column(top_left, pad=((150,0),(0,0))), sg.Push(), sg.Table(
+                                                                    values=[],
+                                                                    headings=predictions_headings, 
+                                                                    max_col_width=35, 
+                                                                    auto_size_columns=True, 
+                                                                    justification='center',
+                                                                    num_rows=10,
+                                                                    key='-TABLE-',
+                                                                    row_height=35,
+                                                                    tooltip='Predictions Table'
+                                                                    )
+                                    ], 
+                                    [sg.Column(bottom_left), sg.Push(), sg.Column(bottom_right)],
+                                    [sg.Button('Connect', size=(8,2), image_filename="./images/connect.png"), sg.Push(),],                               
 
                                   ]
 
     brainwave_prediction_window = sg.Window("Brainwave Prediction", brainwave_prediction_layout, size=(1200, 800), element_justification='c')
+    
 
     count = 0
     predictions_list = ['backward', 'down', 'forward', 'land', 'left', 'right', 'takeoff', 'up']
@@ -133,18 +162,33 @@ def brainwave_prediction_window():
         elif event == "Read my mind...":
             prediction_response = use_brainflow()
             count = prediction_response['prediction_count']
-            prediction_label = prediction_response['prediction_label']
-            # count+=1           
-            brainwave_prediction_window["-COUNT-"].update(count)
-            brainwave_prediction_window["-PREDICTION-"].update(prediction_label)
-            # action_index = predictions_list[count%len(predictions_list)]
-            # action_index = predictions_list.index(action_index)
+            prediction_label = prediction_response['prediction_label']        
+            #brainwave_prediction_window["-COUNT-"].update(count)
+            #brainwave_prediction_window["-PREDICTION-"].update(prediction_label)
 
+            server_record = [[count, prediction_label]]
+            brainwave_prediction_window['-SERVER_TABLE-'].update(values=server_record)
         elif event == "Not what I was thinking...":
             print("you pushed NOT")
         elif event == "Execute":
-            execute = get_drone_action(prediction_label)
-            print(execute)
+            flight_log.insert(0, prediction_label)
+            brainwave_prediction_window['LOG'].update(values=flight_log)
+            get_drone_action(prediction_label)
+            #execute = get_drone_action(prediction_label)
+            print("done")
+            flight_log.insert(0, "done")
+            brainwave_prediction_window['LOG'].update(values=flight_log)
+            prediction_record = [len(predictions_log)+1, count, prediction_label]
+            predictions_log.append(prediction_record)
+            brainwave_prediction_window['-TABLE-'].update(values=predictions_log)
+        elif event == 'Connect':
+            # Code for Connect
+            flight_log.insert(0, "Connect button pressed")
+            brainwave_prediction_window['LOG'].update(values=flight_log)
+            get_drone_action('connect')
+            flight_log.insert(0, "Done.")
+            brainwave_prediction_window['LOG'].update(values=flight_log)
+
     window1.un_hide()
     brainwave_prediction_window.close() 
 
@@ -188,86 +232,100 @@ def manual_drone_control(items):
             break
         elif event == 'Up':
             # Code for moving the drone up
+            get_drone_action('up')
             items.insert(0, "Up button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('up'))
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
-
         elif event == 'Down':
             # Code for moving the drone down                
             items.insert(0, "Down button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('down'))
+            #items.insert(0, get_drone_action('down'))
+            get_drone_action('down')
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Forward':
             # Code for moving the drone forward
             items.insert(0, "Forward button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('forward'))
+            #items.insert(0, get_drone_action('forward'))
+            get_drone_action('forward')
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Back':
             # Code for moving the drone back                
             items.insert(0, "Back button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('backward'))
+            #items.insert(0, get_drone_action('backward'))
+            get_drone_action('backward')
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Left':
             # Code for moving the drone left
             items.insert(0, "Left button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('left'))
+            #items.insert(0, get_drone_action('left'))
+            get_drone_action('left')
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Right':
             # Code for moving the drone right
             items.insert(0, "Right button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('right'))
+            #items.insert(0, get_drone_action('right'))
+            get_drone_action('right')
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Turn Left':
             # Code for turning the drone left
             items.insert(0, "Turn Left button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('turn_left'))
+            #items.insert(0, get_drone_action('turn_left'))
+            get_drone_action('turn_left')
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Turn Right':
             # Code for turning the drone right
             items.insert(0, "Turn right button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('turn_right'))
+            #items.insert(0, get_drone_action('turn_right'))
+            get_drone_action('turn_right')
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Takeoff':
             # Code for taking off the drone
             items.insert(0, "Takeoff button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('takeoff'))
+            get_drone_action('takeoff')
+            items.insert(0, "Done.")
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Land':
             # Code for landing the drone
             items.insert(0, "Land button pressed")
             manual_drone_control_window['LOG'].update(values=items)
-            items.insert(0, get_drone_action('land'))
+            #items.insert(0, get_drone_action('land'))
+            get_drone_action('land')
+            items.insert(0, 'done')
             manual_drone_control_window['LOG'].update(values=items)
         elif event == 'Home':
             # Code for Home
-            items.insert(0, "home testing")
             manual_drone_control_window.close()
             window1.un_hide()
             break               
         elif event == 'Connect':
             # Code for Connect
             items.insert(0, "Connect button pressed")
-
             manual_drone_control_window['LOG'].update(values=items)
-            #tello.connect()
-            time.sleep(2)
+            tello.connect()
             items.insert(0, "Done.")
             manual_drone_control_window['LOG'].update(values=items)
-            #TODO check on connect feedback and if more response is needed here
     
 
     # items.insert(0, "---------- END LOG ----------")
     window1.un_hide
     manual_drone_control_window.close()
+
 
 
 
