26a0227b5f74bcc32499f2a9a5ad9e36a890efbc
KINGTUT10101 <45105509+KINGTUT10101@users.noreply.github.com>
2023-10-03 15:55:56 -0500

    Improve exception handling and add option to ignore host keys

--- brainwave-prediction-app/gui_windows/transfer_files_window.py
+++ brainwave-prediction-app/gui_windows/transfer_files_window.py
@@ -16,6 +16,7 @@ def transfer_files_window(window1):
         [sg.InputText(key="-PRIVATE_KEY_PASS-", enable_events=True, password_char='*')],
         [sg.Text("Private Key Directory:")],
         [sg.InputText(key="-PRIVATE_KEY-", enable_events=True), sg.FileBrowse()],
+        [sg.Checkbox("Ignore Host Key", key="-IGNORE_HOST_KEY-", default=True)],
         [sg.Text("Source Directory:")],
         [sg.InputText(key="-SOURCE-", enable_events=True), sg.FolderBrowse()],
         [sg.Text("Target Directory:")],
@@ -31,20 +32,27 @@ def transfer_files_window(window1):
         if event in (sg.WIN_CLOSED, 'Quit') or event == "Cancel":
             break
         elif event == "Upload":
-            svrcon = fileTransfer(values["-HOST-"], values["-USERNAME-"], values["-PRIVATE_KEY-"], values["-PRIVATE_KEY_PASS-"], True)
-            source_dir = values["-SOURCE-"]
-            target_dir = values["-TARGET-"]
+            svrcon = None
+            source_dir = None
+            target_dir = None
+            
+            try:
+                svrcon = fileTransfer(values["-HOST-"], values["-USERNAME-"], values["-PRIVATE_KEY-"], values["-PRIVATE_KEY_PASS-"], values["-IGNORE_HOST_KEY-"])
+                source_dir = values["-SOURCE-"]
+                target_dir = values["-TARGET-"]
 
-            # Check if both source and target directories are provided
-            if source_dir and target_dir:
-                try:
-                    # Run the sftp.py script
-                    svrcon.transfer(str(source_dir), (target_dir))
-                    sg.popup("File Upload Completed!")
-                except Exception as e:
-                    sg.popup_error(f"Error during upload: {str(e)}")
-            else:
-                sg.popup_error("Please select both source and target directories!")
+                # Check if both source and target directories are provided
+                if source_dir and target_dir:
+                    try:
+                        # Run the sftp.py script
+                        svrcon.transfer(str(source_dir), (target_dir))
+                        sg.popup("File Upload Completed!")
+                    except Exception as e:
+                        sg.popup_error(f"Error during upload: {str(e)}")
+                else:
+                    sg.popup_error("Please select both source and target directories!")
+            except Exception as e:
+                sg.popup_error(f"Error during upload (check your login info): {str(e)}")
 
     window1.un_hide()
     transfer_files_window.close() 
