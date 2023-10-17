2752cdfccaabe9de10a8cabfca8ec2d943a976e3
Kris Heck <kris.heck@go.stcloudstate.edu>
2023-10-17 16:03:51 -0500

    Added remaining GUI elements, removed input element from private key field.

--- brainwave-prediction-app/gui_windows/transfer_data_window.py
+++ brainwave-prediction-app/gui_windows/transfer_data_window.py
@@ -4,8 +4,12 @@ def transfer_data_window():
     layout = [
         [sg.Text("Host:"), sg.Input(s=15)],
         [sg.Text("Username:"), sg.Input(s=15)],
-        [sg.Text("Private Key:"), sg.Input(s=15), sg.FileBrowse()],
+        [sg.Text("Private Key:"), sg.FileBrowse(), sg.Text("")],
         [sg.Text("Password:"), sg.Input(s=15, password_char='*')],
+        [sg.HSep()],
+        [sg.FileBrowse("Select Files"), sg.Text("")],
+        [sg.VPush()],
+        [sg.Button("Send"), sg.Button("Cancel")],
     ]
 
     window = sg.Window("Transfer Data", layout, size=(300, 400), element_justification='c')
