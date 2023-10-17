067be1c35d0652e4d290ed120788985103e1c554
Kris Heck <kris.heck@go.stcloudstate.edu>
2023-10-17 17:24:51 -0500

    Changed login_field_layout* variable names to login_layout*, pressing Cancel now closes the window.

--- brainwave-prediction-app/gui_windows/transfer_data_window.py
+++ brainwave-prediction-app/gui_windows/transfer_data_window.py
@@ -1,26 +1,26 @@
 import PySimpleGUI as sg
 
 def transfer_data_window():
-    login_field_layout_left = [
+    login_layout_left = [
         [sg.Text("Host:")],
         [sg.Text("Username:")],
         [sg.Text("Private Key:")],
         [sg.Text("Password:")],
     ]
 
-    login_field_layout_right = [
+    login_layout_right = [
         [sg.Input(s=25)],
         [sg.Input(s=25)],
         [sg.FileBrowse(), sg.Text("")],
         [sg.Input(s=25, password_char='*')],
     ]
 
-    login_field_layout = [
-        [sg.Col(login_field_layout_left), sg.VPush(), sg.Col(login_field_layout_right)],
+    login_layout = [
+        [sg.Col(login_layout_left), sg.VPush(), sg.Col(login_layout_right)],
     ]
 
     layout = [
-        [sg.Frame("Login", login_field_layout)],
+        [sg.Frame("Login", login_layout)],
         [sg.Text("No Folder Selected")],
         [sg.FileBrowse("Select Folder"), sg.Push(), sg.Button("Send"), sg.Button("Cancel")],
     ]
@@ -30,5 +30,7 @@ def transfer_data_window():
     while True:
         event, values = window.read()
 
-        if event == sg.WIN_CLOSED:
+        if event in [sg.WIN_CLOSED, "Cancel"]:
             break
+
+    window.close()
