e2df3204992c4a128aa91cc99950a52c6ff9e0c9
Kris Heck <kris.heck@go.stcloudstate.edu>
2023-10-17 17:37:16 -0500

    Moved key selection text to left of button, trimmed some extra long lines.

--- brainwave-prediction-app/gui_windows/transfer_data_window.py
+++ brainwave-prediction-app/gui_windows/transfer_data_window.py
@@ -11,18 +11,23 @@ def transfer_data_window():
     login_layout_right = [
         [sg.Input(s=25)],
         [sg.Input(s=25)],
-        [sg.FileBrowse(), sg.Text("")],
+        [sg.Text(""), sg.FileBrowse()],
         [sg.Input(s=25, password_char='*')],
     ]
 
     login_layout = [
-        [sg.Col(login_layout_left), sg.Push(), sg.Col(login_layout_right)],
+        [sg.Col(login_layout_left),
+         sg.Push(),
+         sg.Col(login_layout_right, element_justification="r")],
     ]
 
     layout = [
         [sg.Frame("Login", login_layout)],
         [sg.Text("No Folder Selected")],
-        [sg.FileBrowse("Select Folder"), sg.Push(), sg.Button("Send"), sg.Button("Cancel")],
+        [sg.FileBrowse("Select Folder"),
+         sg.Push(),
+         sg.Button("Send"),
+         sg.Button("Cancel")],
     ]
 
     window = sg.Window("Transfer Data", layout, size=(350, 200), element_justification='c')
