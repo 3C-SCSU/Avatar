199a63821faaeaf48a98d8cf26ee367b2e92fa74
Beteab <beteabtefera@yahoo.com>
2023-10-18 18:11:39 -0500

    Final Commit

--- react/wireframe-homescreen/src/pages/manualcontrol.jsx
+++ react/wireframe-homescreen/src/pages/manualcontrol.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-
+import avatar_logo from '../assets/avatar_logo.png';
 //this component is used for manual dron control page
 
 const ManualControl = () => {
   return (
     <div>
+        <Link to="/">
+            <img style={{width: '75%', height: '150%', borderRadius: 4}} src={avatar_logo} alt='Avatar logo' />
+        </Link>
       <h1>Manual Drone Control</h1>
+      <h3>Page for Manual Drone Control Component</h3>
     </div>
   );
 }
--- react/wireframe-homescreen/src/pages/pilot.jsx
+++ react/wireframe-homescreen/src/pages/pilot.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-
+import avatar_logo from '../assets/avatar_logo.png';
 //this component is used for the pilot page
 
 const Piloting = () => {
   return (
     <div>
+        <Link to="/">
+            <img style={{width: '75%', height: '150%', borderRadius: 4}} src={avatar_logo} alt='Avatar logo' />
+        </Link>
+
       <h1>Brainwave Piloting</h1>
+      <h3>Page for Brainwave Piloting Component</h3>
     </div>
   );
 }
+
 
 export default Piloting;
