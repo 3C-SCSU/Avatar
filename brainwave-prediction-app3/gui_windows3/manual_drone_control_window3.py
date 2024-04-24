85d7fd3fed7a122367a38a68587a32ae1f17cf91
Kingtut 101 <kingtut10101@gmail.com>
2024-04-24 14:35:11 -0500

    Fix imagesnot appearing in manual control window

--- brainwave-prediction-app3/gui_windows3/manual_drone_control_window3.py
+++ brainwave-prediction-app3/gui_windows3/manual_drone_control_window3.py
@@ -19,7 +19,7 @@ class ManDroneCont_Tab(QWidget):
                 else:
                         button.setMinimumSize(QSize(150, 150))
                 button.clicked.connect(lambda: self.button_pressed.emit(name))
-                button.setStyleSheet(f"background-image: url(images3/{name}.png);")
+                button.setStyleSheet(f"background-image: url(brainwave-prediction-app3/images3/{name}.png);")
                 return button
 
         def initUI(self):
