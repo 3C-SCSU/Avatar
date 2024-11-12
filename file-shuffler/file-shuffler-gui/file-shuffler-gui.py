4627a27ff77b3d5c58859506a986f312f71d9861
kygm <rickev20@gmail.com>
2024-11-12 17:49:23 -0600

    added gui and finished up command line logic

--- file-shuffler/file-shuffler-gui/file-shuffler-gui.py
+++ file-shuffler/file-shuffler-gui/file-shuffler-gui.py
@@ -2,13 +2,30 @@
 import sys
 from pathlib import Path
 
+import subprocess
+
+from PySide6.QtCore import QObject, Slot
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
+class FileShufflerGui(QObject):
+    def __init__(self):
+        super().__init__()
+        
+    @Slot(str)
+    def run_file_shuffler_program(self, id):
+        process = subprocess.Popen(["python", "../run_file_shuffler.py"], stdout=subprocess.PIPE, shell=True)
+        (file_shuffler_output, err) = process.communicate()
+
+        return file_shuffler_output
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
+
+    bindingContext = FileShufflerGui()
+    engine.rootContext().setContextProperty("fileShufflerGui", bindingContext)
+
     qml_file = Path(__file__).resolve().parent / "main.qml"
     engine.load(qml_file)
     if not engine.rootObjects():
