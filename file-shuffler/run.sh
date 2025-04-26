48891423d85891865454aa7a242c24d5ace71221
FatedZenith <FatedZenith@outlook.com>
2025-04-26 15:12:55 -0600

    Update run.sh
    Group 10. Fixed some issues related to pathing/directory selection and finding the necessary files on Windows. Ease of use improved.

--- file-shuffler/run.sh
+++ file-shuffler/run.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/bash
 #Authors: Brady Theisen
 
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 echo "Running python script"
-python3 python.py
+python_cmd=$(command -v python3 || command -v python)
+"$python_cmd" "$SCRIPT_DIR/python.py"
+
 echo "Finished running python script"
 
 echo "Running bash script"
-bash bash.sh
+bash "$SCRIPT_DIR/bash.sh"
 echo "Finished running bash script"
