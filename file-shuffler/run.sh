91b15402e3ca2f43c3340c71f464e2542becd330
3C Cloud Computing Club <114175379+3C-SCSU@users.noreply.github.com>
2025-04-28 14:00:12 -0500

    Revert "Update run.sh"

--- file-shuffler/run.sh
+++ file-shuffler/run.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/bash
 #Authors: Brady Theisen
 
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 echo "Running python script"
-python_cmd=$(command -v python3 || command -v python)
-"$python_cmd" "$SCRIPT_DIR/python.py"
-
+python3 python.py
 echo "Finished running python script"
 
 echo "Running bash script"
-bash "$SCRIPT_DIR/bash.sh"
+bash bash.sh
 echo "Finished running bash script"
