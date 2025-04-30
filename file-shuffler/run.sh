371e73f97af41a0fe5c9892fb961309e760e7a54
Jason D <FatedZenith@outlook.com>
2025-04-30 08:12:16 -0600

    Update run.sh

--- file-shuffler/run.sh
+++ file-shuffler/run.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/bash
 #Authors: Brady Theisen
 
+TRUE_PATH="$1"
+
+echo "Path passed from Python: $TRUE_PATH"
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 echo "Running python script"
-python3 python.py
+python_cmd=$(command -v python3 || command -v python)
+"$python_cmd" "$SCRIPT_DIR/python.py" "$TRUE_PATH"
+
 echo "Finished running python script"
 
 echo "Running bash script"
-bash bash.sh
+bash "$SCRIPT_DIR/bash.sh" "$TRUE_PATH"
 echo "Finished running bash script"
