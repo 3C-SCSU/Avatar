cd2c70d916d72165fcf05389f8102a16baef52e1
Jason D <FatedZenith@outlook.com>
2025-10-30 12:31:58 -0500

    Revert "Merge pull request #458 from 3C-SCSU/ManualDroneFix"
    This reverts commit c4998f23d8a11a6aeec664e46313f878730c8ff7, reversing
    changes made to 8a3ef49b6b918711711105dad872973c451fa17e.

--- GUI5.py
+++ GUI5.py
@@ -209,7 +209,6 @@ class BrainwavesBackend(QObject):
         except Exception as e:
             print(f"hofCharts.main() ERROR: {e}")
 
-    #might be useless because is.connected is defined already in the def__init method
     def get_is_connected(self):
         return self.isConnectedChanged
 
@@ -233,7 +232,6 @@ class BrainwavesBackend(QObject):
         self.is_connected = False
         try:
             self.tello = Tello()
-            self.connected = False
         except Exception as e:
             print(f"Warning: Failed to initialize Tello drone: {e}")
             self.logMessage.emit(f"Warning: Failed to initialize Tello drone: {e}")
@@ -420,18 +418,19 @@ class BrainwavesBackend(QObject):
 
     @Slot(str)
     def getDroneAction(self, action):
-        threading.Thread(target=self._doAction, args=(action,), daemon=True).start()
-
-    def _doAction(self, action):
-        try:
-            if action == 'connect':
+        if action == 'connect':
+            try:
                 self.tello.connect()
-                self.connected = True
+                self.is_connected = True
                 self.logMessage.emit("Connected to Tello Drone")
-            elif self.connected:
-                self.logMessage.emit("Drone did not connect.")
-                return
-
+            except:
+                self.is_connected = False
+                self.logMessage.emit("Error during connect: {e}")
+            return
+        elif not self.is_connected:
+            self.logMessage.emit(f"Drone not connected. Aborting command '{action}'.")
+            return
+        try:
             if action == 'up':
                 self.tello.move_up(30)
                 self.logMessage.emit("Moving up")
@@ -455,7 +454,7 @@ class BrainwavesBackend(QObject):
                 self.logMessage.emit("Rotating left")
             elif action == "turn_right":
                 self.tello.rotate_clockwise(45)
-                self.logMessage.emit("Rotating right")
+                self.logMessage.emit("Rotationg right")
             elif action == 'takeoff':
                 self.tello.takeoff()
                 self.logMessage.emit("Taking off")
