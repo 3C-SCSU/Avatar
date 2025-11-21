9097f65347b85bdda1d725bd315b2c35f16abfe8
Madison Arndt <themadhoodSurvival@gmail.com>
2025-11-21 10:44:21 -0600

    Add files via upload

---
+++ InstallModuals.py
@@ -0,0 +1,44 @@
+#program:       InstallModuals
+#purpose:       use pip to install moduals requierd to run the aplication
+#progamer:      Madison Arndt 1/24/2024
+
+
+import pip
+
+importlsl = ['PySimpleGUI',
+             'opencv-python',
+             "paramiko<3.0",
+             'pandas',
+             'requests',
+             'brainflow',
+             'pysftp',
+             'djitellopy',
+             'setuptools',
+             'PyQt5',
+             "PySide6",
+             "pdf2image",
+             "serial",
+             "pyserial",
+             "pysftp"]
+
+
+for i in importlsl:
+    try:
+        pip.main(['install', i])
+        print()
+    except Exception as e:
+        print(f"faild to install {i}\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
